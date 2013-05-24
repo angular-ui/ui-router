1f8f2a6bc222e67882cfad8ed128edd7f29955db
Karsten Sperling <mail@ksperling.net>
2013-05-25 02:45:21 +1200

    Automatic gh-pages build

--- build/angular-ui-states.js
+++ build/angular-ui-states.js
@@ -1,6 +1,6 @@
 /**
  * State-based routing for AngularJS
- * @version v0.0.1 - 2013-05-23
+ * @version v0.0.1 - 2013-05-25
  * @link http://angular-ui.github.com/
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
