1a9623e7c19efbea80fccb3de2d5b5595a675cac
Nate Abele <nate.abele@gmail.com>
2013-07-23 12:13:50 -0500

    HTML5-mode compatibility + tests, fixes #276  - Refactoring directive test event handling

--- test/stateDirectivesSpec.js
+++ test/stateDirectivesSpec.js
@@ -13,7 +13,41 @@ describe('uiStateRef', function() {
   }));
 
   describe('links', function() {
-    var el, scope;
+    var el, scope, document;
+
+    beforeEach(inject(function($document) {
+      document = $document[0];
+    }));
+
+    function triggerClick(el, options) {
+      options = angular.extend({
+        metaKey:  false,
+        ctrlKey:  false,
+        shiftKey: false,
+        altKey:   false,
+        button:   0
+      }, options || {});
+
+      var e = document.createEvent("MouseEvents");
+      e.initMouseEvent(
+        "click", // typeArg of type DOMString, Specifies the event type.
+        true, // canBubbleArg of type boolean, Specifies whether or not the event can bubble.
+        true, // cancelableArg of type boolean, Specifies whether or not the event's default action can be prevented.
+        undefined, // viewArg of type views::AbstractView, Specifies the Event's AbstractView.
+        0, // detailArg of type long, Specifies the Event's mouse click count.
+        0, // screenXArg of type long, Specifies the Event's screen x coordinate
+        0, // screenYArg of type long, Specifies the Event's screen y coordinate
+        0, // clientXArg of type long, Specifies the Event's client x coordinate
+        0, // clientYArg of type long, Specifies the Event's client y coordinate
+        options.ctrlKey, // ctrlKeyArg of type boolean, Specifies whether or not control key was depressed during the Event.
+        options.altKey, // altKeyArg of type boolean, Specifies whether or not alt key was depressed during the Event.
+        options.shiftKey, // shiftKeyArg of type boolean, Specifies whether or not shift key was depressed during the Event.
+        options.metaKey, // metaKeyArg of type boolean, Specifies whether or not meta key was depressed during the Event.
+        options.button, // buttonArg of type unsigned short, Specifies the Event's mouse button.
+        null // relatedTargetArg of type EventTarget
+      );
+      el[0].dispatchEvent(e);
+    }
 
     beforeEach(inject(function($rootScope, $compile) {
       el = angular.element('<a ui-sref="contacts.item.detail({ id: contact.id })">Details</a>');
@@ -27,66 +61,29 @@ describe('uiStateRef', function() {
 
 
     it('should generate the correct href', function() {
-      expect(el.attr('href')).toBe('/contacts/5');
+      expect(el.attr('href')).toBe('#/contacts/5');
     });
 
     it('should update the href when parameters change', function() {
-      expect(el.attr('href')).toBe('/contacts/5');
+      expect(el.attr('href')).toBe('#/contacts/5');
       scope.contact.id = 6;
       scope.$apply();
-      expect(el.attr('href')).toBe('/contacts/6');
+      expect(el.attr('href')).toBe('#/contacts/6');
     });
 
     it('should transition states when left-clicked', inject(function($state, $stateParams, $document, $q) {
       expect($state.$current.name).toEqual('');
 
-      var e = $document[0].createEvent("MouseEvents");
-      e.initMouseEvent(
-        "click", // typeArg of type DOMString, Specifies the event type.
-        true, // canBubbleArg of type boolean, Specifies whether or not the event can bubble.
-        true, // cancelableArg of type boolean, Specifies whether or not the event's default action can be prevented.
-        undefined, // viewArg of type views::AbstractView, Specifies the Event's AbstractView.
-        0, // detailArg of type long, Specifies the Event's mouse click count.
-        0, // screenXArg of type long, Specifies the Event's screen x coordinate
-        0, // screenYArg of type long, Specifies the Event's screen y coordinate
-        0, // clientXArg of type long, Specifies the Event's client x coordinate
-        0, // clientYArg of type long, Specifies the Event's client y coordinate
-        false, // ctrlKeyArg of type boolean, Specifies whether or not control key was depressed during the Event.
-        false, // altKeyArg of type boolean, Specifies whether or not alt key was depressed during the Event.
-        false, // shiftKeyArg of type boolean, Specifies whether or not shift key was depressed during the Event.
-        false, // metaKeyArg of type boolean, Specifies whether or not meta key was depressed during the Event.
-        0, // buttonArg of type unsigned short, Specifies the Event's mouse button.
-        null // relatedTargetArg of type EventTarget
-      );
-      el[0].dispatchEvent(e);
-
+      triggerClick(el);
       $q.flush();
+
       expect($state.current.name).toEqual('contacts.item.detail');
       expect($stateParams).toEqual({ id: "5" });
     }));
 
     it('should not transition states when ctrl-clicked', inject(function($state, $stateParams, $document, $q) {
       expect($state.$current.name).toEqual('');
-
-      var e = $document[0].createEvent("MouseEvents");
-      e.initMouseEvent(
-        "click", // typeArg of type DOMString, Specifies the event type.
-        true, // canBubbleArg of type boolean, Specifies whether or not the event can bubble.
-        true, // cancelableArg of type boolean, Specifies whether or not the event's default action can be prevented.
-        undefined, // viewArg of type views::AbstractView, Specifies the Event's AbstractView.
-        0, // detailArg of type long, Specifies the Event's mouse click count.
-        0, // screenXArg of type long, Specifies the Event's screen x coordinate
-        0, // screenYArg of type long, Specifies the Event's screen y coordinate
-        0, // clientXArg of type long, Specifies the Event's client x coordinate
-        0, // clientYArg of type long, Specifies the Event's client y coordinate
-        true, // ctrlKeyArg of type boolean, Specifies whether or not control key was depressed during the Event.
-        false, // altKeyArg of type boolean, Specifies whether or not alt key was depressed during the Event.
-        false, // shiftKeyArg of type boolean, Specifies whether or not shift key was depressed during the Event.
-        false, // metaKeyArg of type boolean, Specifies whether or not meta key was depressed during the Event.
-        0, // buttonArg of type unsigned short, Specifies the Event's mouse button.
-        null // relatedTargetArg of type EventTarget
-      );
-      el[0].dispatchEvent(e);
+      triggerClick(el, { ctrlKey: true });
 
       $q.flush();
       expect($state.current.name).toEqual('');
@@ -96,27 +93,9 @@ describe('uiStateRef', function() {
     it('should not transition states when meta-clicked', inject(function($state, $stateParams, $document, $q) {
       expect($state.$current.name).toEqual('');
 
-      var e = $document[0].createEvent("MouseEvents");
-      e.initMouseEvent(
-        "click", // typeArg of type DOMString, Specifies the event type.
-        true, // canBubbleArg of type boolean, Specifies whether or not the event can bubble.
-        true, // cancelableArg of type boolean, Specifies whether or not the event's default action can be prevented.
-        undefined, // viewArg of type views::AbstractView, Specifies the Event's AbstractView.
-        0, // detailArg of type long, Specifies the Event's mouse click count.
-        0, // screenXArg of type long, Specifies the Event's screen x coordinate
-        0, // screenYArg of type long, Specifies the Event's screen y coordinate
-        0, // clientXArg of type long, Specifies the Event's client x coordinate
-        0, // clientYArg of type long, Specifies the Event's client y coordinate
-        false, // ctrlKeyArg of type boolean, Specifies whether or not control key was depressed during the Event.
-        false, // altKeyArg of type boolean, Specifies whether or not alt key was depressed during the Event.
-        false, // shiftKeyArg of type boolean, Specifies whether or not shift key was depressed during the Event.
-        true, // metaKeyArg of type boolean, Specifies whether or not meta key was depressed during the Event.
-        0, // buttonArg of type unsigned short, Specifies the Event's mouse button.
-        null // relatedTargetArg of type EventTarget
-      );
-      el[0].dispatchEvent(e);
-
+      triggerClick(el, { metaKey: true });
       $q.flush();
+
       expect($state.current.name).toEqual('');
       expect($stateParams).toEqual({ id: "5" });
     }));
@@ -124,27 +103,9 @@ describe('uiStateRef', function() {
     it('should not transition states when shift-clicked', inject(function($state, $stateParams, $document, $q) {
       expect($state.$current.name).toEqual('');
 
-      var e = $document[0].createEvent("MouseEvents");
-      e.initMouseEvent(
-        "click", // typeArg of type DOMString, Specifies the event type.
-        true, // canBubbleArg of type boolean, Specifies whether or not the event can bubble.
-        true, // cancelableArg of type boolean, Specifies whether or not the event's default action can be prevented.
-        undefined, // viewArg of type views::AbstractView, Specifies the Event's AbstractView.
-        0, // detailArg of type long, Specifies the Event's mouse click count.
-        0, // screenXArg of type long, Specifies the Event's screen x coordinate
-        0, // screenYArg of type long, Specifies the Event's screen y coordinate
-        0, // clientXArg of type long, Specifies the Event's client x coordinate
-        0, // clientYArg of type long, Specifies the Event's client y coordinate
-        false, // ctrlKeyArg of type boolean, Specifies whether or not control key was depressed during the Event.
-        false, // altKeyArg of type boolean, Specifies whether or not alt key was depressed during the Event.
-        true, // shiftKeyArg of type boolean, Specifies whether or not shift key was depressed during the Event.
-        false, // metaKeyArg of type boolean, Specifies whether or not meta key was depressed during the Event.
-        0, // buttonArg of type unsigned short, Specifies the Event's mouse button.
-        null // relatedTargetArg of type EventTarget
-      );
-      el[0].dispatchEvent(e);
-
+      triggerClick(el, { shiftKey: true });
       $q.flush();
+
       expect($state.current.name).toEqual('');
       expect($stateParams).toEqual({ id: "5" });
     }));
@@ -152,27 +113,9 @@ describe('uiStateRef', function() {
     it('should not transition states when middle-clicked', inject(function($state, $stateParams, $document, $q) {
       expect($state.$current.name).toEqual('');
 
-      var e = $document[0].createEvent("MouseEvents");
-      e.initMouseEvent(
-        "click", // typeArg of type DOMString, Specifies the event type.
-        true, // canBubbleArg of type boolean, Specifies whether or not the event can bubble.
-        true, // cancelableArg of type boolean, Specifies whether or not the event's default action can be prevented.
-        undefined, // viewArg of type views::AbstractView, Specifies the Event's AbstractView.
-        0, // detailArg of type long, Specifies the Event's mouse click count.
-        0, // screenXArg of type long, Specifies the Event's screen x coordinate
-        0, // screenYArg of type long, Specifies the Event's screen y coordinate
-        0, // clientXArg of type long, Specifies the Event's client x coordinate
-        0, // clientYArg of type long, Specifies the Event's client y coordinate
-        false, // ctrlKeyArg of type boolean, Specifies whether or not control key was depressed during the Event.
-        false, // altKeyArg of type boolean, Specifies whether or not alt key was depressed during the Event.
-        false, // shiftKeyArg of type boolean, Specifies whether or not shift key was depressed during the Event.
-        false, // metaKeyArg of type boolean, Specifies whether or not meta key was depressed during the Event.
-        1, // buttonArg of type unsigned short, Specifies the Event's mouse button.
-        null // relatedTargetArg of type EventTarget
-      );
-      el[0].dispatchEvent(e);
-
+      triggerClick(el, { button: 1 });
       $q.flush();
+
       expect($state.current.name).toEqual('');
       expect($stateParams).toEqual({ id: "5" });
     }));
@@ -192,7 +135,7 @@ describe('uiStateRef', function() {
     }));
 
     it('should generate the correct action', function() {
-      expect(el.attr('action')).toBe('/contacts/5');
+      expect(el.attr('action')).toBe('#/contacts/5');
     });
   });
 });
