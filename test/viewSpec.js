df4a268fe94437949e86f2d8106bcf25d800ea28
Nate Abele <nate.abele@gmail.com>
2013-09-25 18:58:10 -0400

    style($view): indentation fixes for viewSpec

--- test/viewSpec.js
+++ test/viewSpec.js
@@ -1,41 +1,41 @@
 describe('view', function () {
 
-	it('should notify when content is loading', inject(function($view, $rootScope) {
-		var event, options, loadConfig = {
-			template: 'Hello!',
-			params: { foo: "bar" },
-			controller: "MyController"
-		};
+  it('should notify when content is loading', inject(function($view, $rootScope) {
+    var event, options, loadConfig = {
+      template: 'Hello!',
+      params: { foo: "bar" },
+      controller: "MyController"
+    };
 
-		$rootScope.$on('$viewContentLoading', function(e, o) {
-			event = e;
-			options = o;
-		});
+    $rootScope.$on('$viewContentLoading', function(e, o) {
+      event = e;
+      options = o;
+    });
 
-		$view.load("custom.view", loadConfig);
+    $view.load("custom.view", loadConfig);
 
-		expect(event.name).toBe('$viewContentLoading');
-		expect(options.template).toBe('Hello!');
-		expect(options.targetView).toBe('custom.view');
-		expect(options.params).toEqual({ foo: "bar" });
-	}));
+    expect(event.name).toBe('$viewContentLoading');
+    expect(options.template).toBe('Hello!');
+    expect(options.targetView).toBe('custom.view');
+    expect(options.params).toEqual({ foo: "bar" });
+  }));
 
-	it('should always return a promise', inject(function($view, $httpBackend, $rootScope) {
-		var result;
-		$httpBackend.expectGET('/partials/test.html').respond("Test content");
+  it('should always return a promise', inject(function($view, $httpBackend, $rootScope) {
+    var result;
+    $httpBackend.expectGET('/partials/test.html').respond("Test content");
 
-		$view.load("custom.view", { templateUrl: '/partials/test.html' }).then(function(template) {
-			result = template;
-		});
-		expect(result).toBeUndefined();
+    $view.load("custom.view", { templateUrl: '/partials/test.html' }).then(function(template) {
+      result = template;
+    });
+    expect(result).toBeUndefined();
 
-		$httpBackend.flush();
-		expect(result.$template).toBe("Test content");
+    $httpBackend.flush();
+    expect(result.$template).toBe("Test content");
 
-		$view.load("custom.view", { template: 'Hello!' }).then(function(template) {
-			result = template;
-		});
-		$rootScope.$digest();
-		expect(result.$template).toBe("Hello!");
-	}));
+    $view.load("custom.view", { template: 'Hello!' }).then(function(template) {
+      result = template;
+    });
+    $rootScope.$digest();
+    expect(result.$template).toBe("Hello!");
+  }));
 });
