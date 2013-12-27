e9943894f416793f7eeb5bd41db99af7154396b6
Nate Abele <nate.abele@gmail.com>
2013-12-27 02:40:38 -0500

    chore(testUtils): implement $animate.flush()
     - Reformat and clean up whitespace

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,51 +1,64 @@
 // Promise testing support
-angular.module('ngMock')
-  .config(function ($provide) {
-    $provide.decorator('$q', function ($delegate, $rootScope) {
-      $delegate.flush = function() {
-        $rootScope.$digest();
-      };
+angular.module('ngMock').config(function ($provide) {
+  $provide.decorator('$q', function ($delegate, $rootScope) {
+    $delegate.flush = function() {
+      $rootScope.$digest();
+    };
 
-      // Add callbacks to the promise that expose the resolved value/error
-      function expose(promise) {
-        // Don't add hooks to the same promise twice (shouldn't happen anyway)
-        if (!promise.hasOwnProperty('$$resolved')) {
-          promise.$$resolved = false;
-          promise.then(function (value) {
-            promise.$$resolved = { success: true, value: value };
-          }, function (error) {
-            promise.$$resolved = { success: false, error: error };
-          });
+    // Add callbacks to the promise that expose the resolved value/error
+    function expose(promise) {
+      // Don't add hooks to the same promise twice (shouldn't happen anyway)
+      if (!promise.hasOwnProperty('$$resolved')) {
+        promise.$$resolved = false;
+        promise.then(function (value) {
+          promise.$$resolved = { success: true, value: value };
+        }, function (error) {
+          promise.$$resolved = { success: false, error: error };
+        });
 
-          // We need to expose() any then()ed promises recursively
-          var qThen = promise.then;
-          promise.then = function () {
-            return expose(qThen.apply(this, arguments));
-          };
-        }
-        return promise;
-      }
-
-      // Wrap functions that return a promise
-      angular.forEach([ 'when', 'all', 'reject'], function (name) {
-        var qFunc = $delegate[name];
-        $delegate[name] = function () {
-          return expose(qFunc.apply(this, arguments));
+        // We need to expose() any then()ed promises recursively
+        var qThen = promise.then;
+        promise.then = function () {
+          return expose(qThen.apply(this, arguments));
         };
-      });
-
-      // Wrap defer()
-      var qDefer = $delegate.defer;
-      $delegate.defer = function () {
-        var deferred = qDefer();
-        expose(deferred.promise);
-        return deferred;
       }
+      return promise;
+    }
 
+    // Wrap functions that return a promise
+    angular.forEach([ 'when', 'all', 'reject'], function (name) {
+      var qFunc = $delegate[name];
+      $delegate[name] = function () {
+        return expose(qFunc.apply(this, arguments));
+      };
+    });
+
+    // Wrap defer()
+    var qDefer = $delegate.defer;
+    $delegate.defer = function () {
+      var deferred = qDefer();
+      expose(deferred.promise);
+      return deferred;
+    }
+
+    return $delegate;
+  });
+});
+
+try {
+  // Animation testing support
+  angular.module('mock.animate').config(function ($provide) {
+    $provide.decorator('$animate', function ($delegate) {
+      $delegate.flush = function() {
+        while (this.queue.length > 0) {
+          this.flushNext(this.queue[0].method);
+        }
+      };
       return $delegate;
     });
   });
-  
+} catch (e) {}
+
 function testablePromise(promise) {
   if (!promise || !promise.then) throw new Error('Expected a promise, but got ' + jasmine.pp(promise) + '.');
   if (!isDefined(promise.$$resolved)) throw new Error('Promise has not been augmented by ngMock');
