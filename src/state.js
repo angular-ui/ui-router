0da45a1e0fe0b3368d47df6951717d8e984a137d
Nate Abele <nate.abele@gmail.com>
2014-03-24 23:44:34 -0400

    refactor($state): simplify include()

--- src/state.js
+++ src/state.js
@@ -1043,29 +1043,20 @@ function $StateProvider(   $urlRouterProvider,   $urlMatcherFactory) {
 
     $state.includes = function includes(stateOrName, params) {
       if (isString(stateOrName) && isGlob(stateOrName)) {
-        if (doesStateMatchGlob(stateOrName)) {
-          stateOrName = $state.$current.name;
-        } else {
+        if (!doesStateMatchGlob(stateOrName)) {
           return false;
         }
+        stateOrName = $state.$current.name;
       }
-
       var state = findState(stateOrName);
+
       if (!isDefined(state)) {
         return undefined;
       }
-
       if (!isDefined($state.$current.includes[state.name])) {
         return false;
       }
-
-      var validParams = true;
-      angular.forEach(params, function(value, key) {
-        if (!isDefined($stateParams[key]) || $stateParams[key] !== value) {
-          validParams = false;
-        }
-      });
-      return validParams;
+      return equalForKeys(params, $stateParams);
     };
 
 
