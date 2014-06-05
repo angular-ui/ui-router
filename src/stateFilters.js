0f1509f555eee85d70d6b958aa022b8627107c28
Raymond Jelierse <raymond@shareworks.nl>
2014-06-05 11:53:18 +0200

    feat(stateFilters): add support for state parameters to the 'isState' and 'includedByState' filters
    Without the support for the parameters argument, there is still a need to expose $state on the
    scope when comparing states on their parameters.

--- src/stateFilters.js
+++ src/stateFilters.js
@@ -6,11 +6,13 @@
  *
  * @description
  * Translates to {@link ui.router.state.$state#methods_is $state.is("stateName")}.
+ *
+ * @param {Object} The params object
  */
 $IsStateFilter.$inject = ['$state'];
 function $IsStateFilter($state) {
-  return function(state) {
-    return $state.is(state);
+  return function(state, params) {
+    return $state.is(state, params);
   };
 }
 
@@ -22,11 +24,13 @@ function $IsStateFilter($state) {
  *
  * @description
  * Translates to {@link ui.router.state.$state#methods_includes $state.includes('fullOrPartialStateName')}.
+ *
+ * @param {Object} The params object
  */
 $IncludedByStateFilter.$inject = ['$state'];
 function $IncludedByStateFilter($state) {
-  return function(state) {
-    return $state.includes(state);
+  return function(state, params) {
+    return $state.includes(state, params);
   };
 }
 
