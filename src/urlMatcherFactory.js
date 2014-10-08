2541023f497d595506ef735baa6671e1f9464385
christopherthielen <github@sandgnat.com>
2014-10-07 21:40:36 -0500

    fix($urlMatcherFactory): always initalize $urlMatcherFactory at runtime.

--- src/urlMatcherFactory.js
+++ src/urlMatcherFactory.js
@@ -753,3 +753,4 @@ function $UrlMatcherFactory() {
 
 // Register as a provider so it's available to other providers
 angular.module('ui.router.util').provider('$urlMatcherFactory', $UrlMatcherFactory);
+angular.module('ui.router.util').run(['$urlMatcherFactory', function($urlMatcherFactory) { }]);
