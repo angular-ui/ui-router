0e41a81992e429a17a7de13b8533b3ce568aaded
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-02-11 20:00:35 -0800

    (chore): Add packaging for Meteor.js

---
+++ package.js
@@ -0,0 +1,20 @@
+// package metadata file for Meteor.js
+var packageName = 'angularui:angular-ui-router'; // https://atmospherejs.com/angularui/angular-ui-router
+var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
+var version = '0.2.13_3';
+
+Package.describe({
+  name: packageName,
+  version: version,
+  summary: 'angular-ui-router (official): Flexible routing with nested views in AngularJS',
+  git: 'git@github.com:angular-ui/ui-router.git',
+  documentation: null
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+
+  api.use('angular:angular@1.0.8', where);
+
+  api.addFiles('release/angular-ui-router.js', where);
+});
