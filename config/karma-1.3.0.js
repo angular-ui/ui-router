42095a5b06253dadccb106a6050b13b34f8f58bd
christopherthielen <github@sandgnat.com>
2015-05-19 07:28:02 -0500

    chore(karma): Added jasmine framework declaration to karma configs Fixes #1965

--- config/karma-1.3.0.js
+++ config/karma-1.3.0.js
@@ -13,6 +13,7 @@ module.exports = function (karma) {
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: karma.LOG_DEBUG,
+    frameworks: ['jasmine'],
 
     // Start these browsers, currently available:
     // - Chrome
