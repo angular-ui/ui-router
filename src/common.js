663c1d7532a41a985f10c74281ccfbafb1e45705
Nate Abele <nate.abele@gmail.com>
2015-06-02 08:27:44 -0400

    fix(common): defaults() should filter option keys

--- src/common.js
+++ src/common.js
@@ -18,7 +18,7 @@ function inherit(parent, extra) {
 }
 
 function defaults(opts, defs) {
-  return extend({}, defs, opts);
+  return extend({}, defs, pick(opts, objectKeys(defs)));
 }
 
 function merge(dst) {
@@ -198,9 +198,9 @@ function indexBy(array, propName) {
 function pick(obj) {
   var copy = {};
   var keys = Array.prototype.concat.apply(Array.prototype, Array.prototype.slice.call(arguments, 1));
-  forEach(keys, function(key) {
-    if (key in obj) copy[key] = obj[key];
-  });
+  for (var key in obj) {
+    if (keys.indexOf(key) !== -1) copy[key] = obj[key];
+  }
   return copy;
 }
 
@@ -210,7 +210,7 @@ function omit(obj) {
   var copy = {};
   var keys = Array.prototype.concat.apply(Array.prototype, Array.prototype.slice.call(arguments, 1));
   for (var key in obj) {
-    if (keys.indexOf(key) == -1) copy[key] = obj[key];
+    if (keys.indexOf(key) === -1) copy[key] = obj[key];
   }
   return copy;
 }
