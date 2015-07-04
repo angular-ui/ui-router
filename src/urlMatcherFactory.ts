fd106280c3c469afaaf384d2990b715a66eff176
Wesley Cho <wesley.cho@gmail.com>
2015-07-04 16:10:34 -0700

    refactor(urlMatcherFactory): convert another string to template string

--- src/urlMatcherFactory.ts
+++ src/urlMatcherFactory.ts
@@ -974,7 +974,7 @@ export class Param {
     }
 
     function getType(config, urlType, location) {
-      if (config.type && urlType) throw new Error("Param '" + id + "' has two type configurations.");
+      if (config.type && urlType) throw new Error(`Param '${id}' has two type configurations.`);
       if (urlType) return urlType;
       if (!config.type) return (location === "config" ? paramTypes.type("any") : paramTypes.type("string"));
       return config.type instanceof Type ? config.type : new Type(config.type);
