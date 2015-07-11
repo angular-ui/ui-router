74453bf06e7ee13056576a1947c4b4aac1edf768
christopherthielen <github@sandgnat.com>
2015-07-11 11:52:21 -0500

    test(viewDirective): Re-add angular-animate.js to specs

--- files.js
+++ files.js
@@ -44,8 +44,9 @@ routerFiles = {
   angular: function(version) {
     return [
       'lib/angular-' + version + '/angular.js',
-      'lib/angular-' + version + '/angular-mocks.js'
-    ].concat(['1.2.14', '1.3.0-rc.1'].indexOf(version) !== -1 ? ['lib/angular-' + version + '/angular-animate.js'] : []);
+      'lib/angular-' + version + '/angular-mocks.js',
+      'lib/angular-' + version + '/angular-animate.js'
+    ];
   }
 };
 
