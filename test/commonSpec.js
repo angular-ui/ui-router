393868b7b510d99def9e78668573e876734b4f3c
christopherthielen <github@sandgnat.com>
2015-07-19 11:26:10 -0500

    chore(*): Update PhantomJS bind shim to use es5-shim's version

--- test/commonSpec.js
+++ test/commonSpec.js
@@ -16,27 +16,44 @@ var common = uiRouter.common,
  * Because PhantomJS sucks...
  */
 if (!Function.prototype.bind) {
-  Function.prototype.bind = function(oThis) {
-    if (typeof this !== 'function') {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+  var Empty = function(){};
+  Function.prototype.bind = function bind(that) { // .length is 1
+    var target = this;
+    if (typeof target != "function") {
+      throw new TypeError("Function.prototype.bind called on incompatible " + target);
     }
+    var args = Array.prototype.slice.call(arguments, 1); // for normal call
+    var binder = function () {
+      if (this instanceof bound) {
+        var result = target.apply(
+          this,
+          args.concat(Array.prototype.slice.call(arguments))
+        );
+        if (Object(result) === result) {
+          return result;
+        }
+        return this;
+      } else {
+        return target.apply(
+          that,
+          args.concat(Array.prototype.slice.call(arguments))
+        );
+      }
+    };
+    var boundLength = Math.max(0, target.length - args.length);
+    var boundArgs = [];
+    for (var i = 0; i < boundLength; i++) {
+      boundArgs.push("$" + i);
+    }
+    var bound = Function("binder", "return function(" + boundArgs.join(",") + "){return binder.apply(this,arguments)}")(binder);
 
-    var aArgs   = Array.prototype.slice.call(arguments, 1),
-        fToBind = this,
-        fNOP    = function() {},
-        fBound  = function() {
-          return fToBind.apply(this instanceof fNOP && oThis
-                 ? this
-                 : oThis,
-                 aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
+    if (target.prototype) {
+      Empty.prototype = target.prototype;
+      bound.prototype = new Empty();
+      // Clean up dangling references.
+      Empty.prototype = null;
+    }
+    return bound;
   };
 }
 
