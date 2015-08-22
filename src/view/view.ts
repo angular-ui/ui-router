d286c7e4c3319d54e211746bf3ac8bd8c948fe62
christopherthielen <github@sandgnat.com>
2015-08-22 17:18:50 -0500

    chore(view): tslint view.ts

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,22 +1,20 @@
 /// <reference path='../../typings/angularjs/angular.d.ts' />
-import {isInjectable, isDefined, isArray, defaults, equals, extend, forEach, map, parse, objectKeys, noop,
-    unnest, prop, pick, pluck, removeFrom, eq, isEq, val, compose, _map, Fx} from "../common/common";
-import PathContext from "../resolve/pathContext";
-import {IDeferred} from "angular";
+import {isInjectable, defaults, extend, unnest, prop, pick, removeFrom, isEq, val, _map, Fx} from "../common/common";
 import {IStateViewConfig} from "../state/interface";
 
-interface ViewKey {
-  context: any,
-  name: string,
-  item: any
+interface IViewKey {
+  context: any;
+  name: string;
+  item: any;
 }
 
-const _debug = false;
-const debug = function(...any) {
+let _debug = false;
+function debug(...any);
+function debug() {
   if (_debug) { console.log.call(console, arguments); }
-};
+}
 
-function viewKey(item: any): ViewKey {
+function viewKey(item: any): IViewKey {
   return extend(pick(item, "name", "context", "parentContext"),  { item: item });
 }
 
@@ -54,12 +52,12 @@ export class ViewConfig {
    * @return {boolean} Returns `true` if the configuration contains a valid template, otherwise `false`.
    */
   hasTemplate() {
-    var viewDef = this.sourceStateConfig.view;
+    let viewDef = this.sourceStateConfig.view;
     return !!(viewDef.template || viewDef.templateUrl || viewDef.templateProvider);
   }
 
   getTemplate($factory) {
-    var locals = this.locals, viewDef = this.sourceStateConfig.view;
+    let locals = this.locals, viewDef = this.sourceStateConfig.view;
     return $factory.fromConfig(viewDef, this.params, locals.invoke.bind(locals));
   }
 
@@ -71,7 +69,7 @@ export class ViewConfig {
    */
   getController() {
     //* @param {Object} locals A context object from transition.context() to invoke a function in the correct context
-    var provider = this.sourceStateConfig.view.controllerProvider;
+    let provider = this.sourceStateConfig.view.controllerProvider;
     return isInjectable(provider) ? this.locals.invoke(provider) : this.sourceStateConfig.view.controller;
   }
 }
@@ -89,8 +87,8 @@ export class ViewConfig {
 $View.$inject = ['$rootScope', '$templateFactory', '$q', '$timeout'];
 function $View(   $rootScope,   $templateFactory,   $q,   $timeout) {
 
-  var uiViews = [];
-  var viewConfigs = [];
+  let uiViews = [];
+  let viewConfigs = [];
 
   const match = (obj1, ...keys) =>
       (obj2) => keys.reduce(((memo, key) => memo && obj1[key] === obj2[key]), true);
@@ -116,7 +114,7 @@ function $View(   $rootScope,   $templateFactory,   $q,   $timeout) {
    * @return {Promise.<string>} Returns a promise that resolves to the value of the template loaded.
    */
   this.load = function load (viewConfig: ViewConfig, options) {
-    var options = defaults(options, {
+    options = defaults(options, {
       context:            null,
       parent:             null,
       notify:             true,
@@ -153,7 +151,7 @@ function $View(   $rootScope,   $templateFactory,   $q,   $timeout) {
       $rootScope.$broadcast('$viewContentLoading', extend({ targetView: name }, options));
     }
 
-    var promises = {
+    let promises = {
       template: $q.when(viewConfig.getTemplate($templateFactory)),
       controller: $q.when(viewConfig.getController())
     };
@@ -170,28 +168,28 @@ function $View(   $rootScope,   $templateFactory,   $q,   $timeout) {
    * @return {Boolean} Returns `true` if the view exists, otherwise `false`.
    */
   this.reset = function reset (stateViewConfig) {
-    var viewConfig = new ViewConfig(stateViewConfig);
+    let viewConfig = new ViewConfig(stateViewConfig);
     viewConfigs.filter(match(viewConfig, "name", "context")).forEach(removeFrom(viewConfigs));
     uiViews.filter(match(viewConfig, "name", "parentContext")).forEach((uiView) => uiView.configUpdated({}));
     this.sync();
   };
 
   this.registerStateViewConfig = function(stateViewConfig: IStateViewConfig) {
-    var viewConfig = new ViewConfig(stateViewConfig);
+    let viewConfig = new ViewConfig(stateViewConfig);
     viewConfigs.push(viewConfig);
     this.load(viewConfig);
   };
 
   this.sync = () => {
-    var viewKeyMapper: Fx<any[],ViewConfig[]> = _map(viewKey);
-    var [uiViewKeys, viewConfigKeys] = [uiViews, viewConfigs].map(viewKeyMapper);
-    var matchingKeyPairs = unnest(uiViewKeys.map((uiView) => {
-      var matches = viewConfigKeys.filter(match(uiView, "name", "parentContext"));
+    let viewKeyMapper: Fx<any[], ViewConfig[]> = _map(viewKey);
+    let [uiViewKeys, viewConfigKeys] = [uiViews, viewConfigs].map(viewKeyMapper);
+    let matchingKeyPairs = unnest(uiViewKeys.map((uiView) => {
+      let matches = viewConfigKeys.filter(match(uiView, "name", "parentContext"));
       return matches.map((config) => [uiView, config]);
     }));
 
     const configureUiView = ([uiView, viewConfig]) => uiView.configUpdated(viewConfig);
-    matchingKeyPairs.map(_map(prop("item"))).forEach(configureUiView)
+    matchingKeyPairs.map(_map(prop("item"))).forEach(configureUiView);
   };
 
   /**
@@ -204,7 +202,7 @@ function $View(   $rootScope,   $templateFactory,   $q,   $timeout) {
    * @return {Function} Returns a de-registration function used when the view is destroyed.
    */
   this.registerUiView = function register(uiView) {
-    var fqnMatches = isEq(prop("fqn"), val(uiView.fqn));
+    let fqnMatches = isEq(prop("fqn"), val(uiView.fqn));
     if (uiViews.filter(fqnMatches).length)
       debug(`uiView already exists with name: '${uiView.fqn}' (named ${uiView.name} in context ${uiView.context})`);
 
@@ -212,7 +210,7 @@ function $View(   $rootScope,   $templateFactory,   $q,   $timeout) {
     this.sync();
 
     return () => {
-      var idx = uiViews.indexOf(uiView);
+      let idx = uiViews.indexOf(uiView);
       if (idx <= 0) {
         debug("Tried removing non-registered uiView");
         return;
@@ -223,7 +221,7 @@ function $View(   $rootScope,   $templateFactory,   $q,   $timeout) {
       debug("uiViews: ", uiViews.map(prop("fqn")));
 
       this.sync();
-    }
+    };
   };
 
   /**
