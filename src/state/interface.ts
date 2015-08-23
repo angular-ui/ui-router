e9e758a784efc32e6c7bcc85006d63013d1d5256
christopherthielen <github@sandgnat.com>
2015-08-22 19:01:33 -0500

    chore(state): tslint state/interface.ts

--- src/state/interface.ts
+++ src/state/interface.ts
@@ -1,29 +1,27 @@
-import {IPromise} from "angular"
+import {IPromise} from "angular";
 
-import UrlMatcher from "../url/urlMatcher"
+import UrlMatcher from "../url/urlMatcher";
 
-import {IRawParams, IParamsOrArray} from "../params/interface"
-import ParamSet from "../params/paramSet"
-import ParamValues from "../params/paramValues"
+import {IRawParams, IParamsOrArray} from "../params/interface";
+import ParamSet from "../params/paramSet";
 
-import {IViewContext} from "../view/interface"
-import PathContext from "../resolve/pathContext"
+import {IViewContext} from "../view/interface";
+import PathContext from "../resolve/pathContext";
 
-import {StateParams} from "./state"
-import TargetState from "./targetState"
+import TargetState from "./targetState";
 
-import {ITransitionOptions} from "../transition/interface"
-import {Transition} from "../transition/transition"
+import {ITransitionOptions} from "../transition/interface";
+import {Transition} from "../transition/transition";
 
 export type IStateOrName = (string|IStateDeclaration|IState);
 
 /** Context obj, State-view definition, transition params */
 export interface IStateViewConfig {
-  view: IViewDeclaration, // A view block from a state config
-  name: string, // The name of the view block
-  params: any // State params?
-  context: IViewContext, // The context object reference
-  pathContext: PathContext, // The Resolve context (rename class!)
+  view: IViewDeclaration; // A view block from a state config
+  name: string; // The name of the view block
+  params: any; // State params?
+  context: IViewContext; // The context object reference
+  pathContext: PathContext; // The Resolve context (rename class!)
 }
 
 /** View declaration inside state declaration */
@@ -36,12 +34,12 @@ export interface IViewDeclaration {
 }
 
 /** hash of strings->views */
-interface IViewDeclarations     { [key:string]: IViewDeclaration; }
+interface IViewDeclarations     { [key: string]: IViewDeclaration; }
 /** hash of strings->resolve fns */
-export interface IResolveDeclarations  { [key:string]: Function; }
+export interface IResolveDeclarations  { [key: string]: Function; }
 /** hash of strings->param declarations */
 // If the value is of type 'any', then it is syntax sugar for an IParamDeclaration { value: value }
-interface IParamsDeclaration    { [key:string]: (IParamDeclaration|any) }
+interface IParamsDeclaration    { [key: string]: (IParamDeclaration|any); }
 /** declaration of single state param */
 interface IParamDeclaration {
   value: any;
@@ -59,8 +57,8 @@ export interface IStateDeclaration extends IViewDeclaration {
   params: IParamsDeclaration;
   views: IViewDeclarations;
   data: any;
-  onEnter: Function,
-  onExit: Function
+  onEnter: Function;
+  onExit: Function;
   // TODO: finish defining state definition API.  Maybe start with what's on Definitely Typed.
 }
 
@@ -84,42 +82,42 @@ export interface IState {
 }
 
 export interface IStateParams {
-  $digest: () => void,
-  $inherit: (newParams, $current: IState, $to: IState) => IStateParams
-  $set: (params, url) => boolean,
-  $sync: () => IStateParams,
-  $off: () => IStateParams,
-  $raw: () => any,
-  $localize: () => IStateParams,
-  $observe: (key, fn) => () => void
+  $digest: () => void;
+  $inherit: (newParams, $current: IState, $to: IState) => IStateParams;
+  $set: (params, url) => boolean;
+  $sync: () => IStateParams;
+  $off: () => IStateParams;
+  $raw: () => any;
+  $localize: () => IStateParams;
+  $observe: (key, fn) => () => void;
 }
 
 export interface IHrefOptions {
-  relative  ?: IStateOrName,
-  lossy     ?: boolean,
-  inherit   ?: boolean,
-  absolute  ?: boolean
+  relative?:  IStateOrName;
+  lossy?:     boolean;
+  inherit?:   boolean;
+  absolute?:  boolean;
 }
 
 export interface IStateProvider {
-  state(state: IStateDeclaration): IStateProvider,
-  state(name: string, state: IStateDeclaration): IStateProvider
-  onInvalid(callback: Function): void,
-  decorator(name: string, func: Function)
+  state(state: IStateDeclaration): IStateProvider;
+  state(name: string, state: IStateDeclaration): IStateProvider;
+  onInvalid(callback: Function): void;
+  decorator(name: string, func: Function);
 }
 
 export interface IStateService {
-  params: any, // TODO: StateParams
-  current: IStateDeclaration,
-  $current: IState,
-  transition: Transition,
-  reload        (stateOrName: IStateOrName): IPromise<IState>,
-  targetState   (identifier: IStateOrName, params: IParamsOrArray, options: ITransitionOptions): TargetState,
-  go            (to: IStateOrName, params: IRawParams, options: ITransitionOptions): IPromise<IState>,
-  transitionTo  (to: IStateOrName, toParams: IParamsOrArray, options: ITransitionOptions): IPromise<IState>,
-  is            (stateOrName: IStateOrName, params?: IRawParams, options?: ITransitionOptions): boolean,
-  includes      (stateOrName: IStateOrName, params?: IRawParams, options?: ITransitionOptions): boolean,
-  href          (stateOrName: IStateOrName, params?: IRawParams, options?: IHrefOptions): string,
-  get           (stateOrName: IStateOrName, base?: IStateOrName): (IStateDeclaration|IStateDeclaration[])
+  params:       any; // TODO: StateParams
+  current:      IStateDeclaration;
+  $current:     IState;
+  transition:   Transition;
+  reload        (stateOrName: IStateOrName): IPromise<IState>;
+  targetState   (identifier: IStateOrName, params: IParamsOrArray, options: ITransitionOptions): TargetState;
+  go            (to: IStateOrName, params: IRawParams, options: ITransitionOptions): IPromise<IState>;
+  transitionTo  (to: IStateOrName, toParams: IParamsOrArray, options: ITransitionOptions): IPromise<IState>;
+  is            (stateOrName: IStateOrName, params?: IRawParams, options?: ITransitionOptions): boolean;
+  includes      (stateOrName: IStateOrName, params?: IRawParams, options?: ITransitionOptions): boolean;
+  href          (stateOrName: IStateOrName, params?: IRawParams, options?: IHrefOptions): string;
+  get           (stateOrName: IStateOrName, base?: IStateOrName): (IStateDeclaration|IStateDeclaration[]);
 }
 
