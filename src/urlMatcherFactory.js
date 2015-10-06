f5d308f1a3418a40ebb34d48e02881350d0084a5
Nate Abele <nate@radify.io>
2015-10-06 17:30:20 -0400

    Revert "feat(UrlMatcher): Add param only type names"

--- src/urlMatcherFactory.js
+++ src/urlMatcherFactory.js
@@ -917,8 +917,7 @@ function $UrlMatcherFactory() {
     }
 
     function getType(config, urlType, location) {
-      if (config.type && urlType.name !== 'string') throw new Error("Param '"+id+"' has two type configurations.");
-      if (config.type && urlType.name === 'string' && $types[config.type]) return $types[config.type];
+      if (config.type && urlType) throw new Error("Param '"+id+"' has two type configurations.");
       if (urlType) return urlType;
       if (!config.type) return (location === "config" ? $types.any : $types.string);
       return config.type instanceof Type ? config.type : new Type(config.type);
