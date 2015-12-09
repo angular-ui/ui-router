d31b3337cc2ce71d87c92fdded629e46558d0b49
Iain MacNeill <Iain.MacNeill@wolterskluwer.com>
2015-12-09 14:32:26 -0500

    fix(urlMatcherFactory.js): change to allow url query param names to contain periods
    This change modifies the searchPlaceholder regex to include the period
    character as valid for url query param names. This change also modifies
    the regex found in the addParameter function which validates the param
    name just prior to adding to the list of parameters. A new set of unit
    tests were added to urlMatcherFactorySpec to ensure this modification
    works correctly.
    
    This change resolves issue #2395

--- test/urlMatcherFactorySpec.js
+++ test/urlMatcherFactorySpec.js
@@ -88,6 +88,28 @@ describe("UrlMatcher", function () {
     });
   });
 
+  describe("parameters containing periods", function() {
+    it("should match if properly formatted", function() {
+      var matcher = new UrlMatcher('/users/?from&to&with.periods&with.periods.also');
+      var params = matcher.parameters();
+
+      expect(params.length).toBe(4);
+      expect(params).toContain('from');
+      expect(params).toContain('to');
+      expect(params).toContain('with.periods');
+      expect(params).toContain('with.periods.also');
+      expect(matcher.parameters()).toEqual(['from','to','with.periods','with.periods.also']);
+    });
+
+    it("should not match if invalid", function() {
+      var err = "Invalid parameter name '.periods' in pattern '/users/?from&to&.periods'";
+      expect(function() { new UrlMatcher('/users/?from&to&.periods'); }).toThrow(err);
+
+      err = "Invalid parameter name 'periods.' in pattern '/users/?from&to&periods.'";
+      expect(function() { new UrlMatcher('/users/?from&to&periods.'); }).toThrow(err);
+    });
+  });  
+
   describe(".exec()", function() {
     it("should capture parameter values", function () {
       var m = new UrlMatcher('/users/:id/details/{type}/{repeat:[0-9]+}?from&to');
