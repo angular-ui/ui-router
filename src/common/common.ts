b083b05d8dc2a6b7d6b5a5b1f67ffffc65c50f0d
christopherthielen <github@sandgnat.com>
2016-01-08 11:34:35 -0600

    refactor(common): re-implement helpers formerly delegated to angular helpers

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,7 +1,19 @@
 /** @module common */ /** for typedoc */
 /// <reference path='../../typings/angularjs/angular.d.ts' />
-let { isDefined, isFunction, isNumber, isString, isObject, isArray, forEach, extend, copy, noop, toJson, fromJson, equals, identity } = angular;
-export { isDefined, isFunction, isNumber, isString, isObject, isArray, forEach, extend, copy, noop, toJson, fromJson, equals, identity };
+import {isDefined, isFunction, isNumber, isString, isObject, isArray, isRegExp, isDate} from "./predicates";
+
+export const fromJson = angular && angular.fromJson || _fromJson;
+export const toJson = angular && angular.toJson || _toJson;
+export const copy = angular && angular.copy || _copy;
+export const forEach = angular && angular.forEach || _forEach;
+export const extend = angular && angular.extend || _extend;
+export const equals = angular && angular.equals || _equals;
+export const identity = (x) => x;
+export const noop = () => undefined;
+
+export * from "./hof";
+import { all, pattern, and, any, not, prop, curry, pipe, val } from "./hof";
+export { isDefined, isFunction, isNumber, isString, isObject, isArray };
 
 type Mapper<X, T> = (x: X, key?: (string|number)) => T;
 export interface TypedMap<T> { [key: string]: T; }
@@ -70,201 +82,6 @@ export function bindFunctions(from, to, bindTo, fnNames: string[] = Object.keys(
       .forEach(name => to[name] = from[name].bind(bindTo));
 }
 
-/**
- * Returns a new function for [Partial Application](https://en.wikipedia.org/wiki/Partial_application) of the original function.
- *
- * Given a function with N parameters, returns a new function that supports partial application.
- * The new function accepts anywhere from 1 to N parameters.  When that function is called with M parameters,
- * where M is less than N, it returns a new function that accepts the remaining parameters.  It continues to
- * accept more parameters until all N parameters have been supplied.
- *
- *
- * This contrived example uses a partially applied function as an predicate, which returns true
- * if an object is found in both arrays.
- * @example
- * ```
- * // returns true if an object is in both of the two arrays
- * function inBoth(array1, array2, object) {
- *   return array1.indexOf(object) !== -1 &&
- *          array2.indexOf(object) !== 1;
- * }
- * let obj1, obj2, obj3, obj4, obj5, obj6, obj7
- * let foos = [obj1, obj3]
- * let bars = [obj3, obj4, obj5]
- *
- * // A curried "copy" of inBoth
- * let curriedInBoth = curry(inBoth);
- * // Partially apply both the array1 and array2
- * let inFoosAndBars = curriedInBoth(foos, bars);
- *
- * // Supply the final argument; since all arguments are
- * // supplied, the original inBoth function is then called.
- * let obj1InBoth = inFoosAndBars(obj1); // false
- *
- * // Use the inFoosAndBars as a predicate.
- * // Filter, on each iteration, supplies the final argument
- * let allObjs = [ obj1, obj2, obj3, obj4, obj5, obj6, obj7 ];
- * let foundInBoth = allObjs.filter(inFoosAndBars); // [ obj3 ]
- *
- * ```
- *
- * Stolen from: http://stackoverflow.com/questions/4394747/javascript-curry-function
- *
- * @param fn
- * @returns {*|function(): (*|any)}
- */
-export function curry(fn: Function): Function {
-  let initial_args = [].slice.apply(arguments, [1]);
-  let func_args_length = fn.length;
-
-  function curried(args) {
-    if (args.length >= func_args_length)
-      return fn.apply(null, args);
-    return function () {
-      return curried(args.concat([].slice.apply(arguments)));
-    };
-  }
-  return curried(initial_args);
-}
-
-/**
- * Given a varargs list of functions, returns a function that composes the argument functions, right-to-left
- * given: f(x), g(x), h(x)
- * let composed = compose(f,g,h)
- * then, composed is: f(g(h(x)))
- */
-export function compose() {
-  let args = arguments;
-  let start = args.length - 1;
-  return function() {
-    let i = start, result = args[start].apply(this, arguments);
-    while (i--) result = args[i].call(this, result);
-    return result;
-  };
-}
-
-/**
- * Given a varargs list of functions, returns a function that is composes the argument functions, left-to-right
- * given: f(x), g(x), h(x)
- * let piped = pipe(f,g,h);
- * then, piped is: h(g(f(x)))
- */
-export function pipe(...funcs: Function[]): (obj: any) => any {
-  return compose.apply(null, [].slice.call(arguments).reverse());
-}
-
-/**
- * Given a property name, returns a function that returns that property from an object
- * let obj = { foo: 1, name: "blarg" };
- * let getName = prop("name");
- * getName(obj) === "blarg"
- */
-export const prop = (name: string) => (obj: any) => obj && obj[name];
-
-/**
- * Given a property name and a value, returns a function that returns a boolean based on whether
- * the passed object has a property that matches the value
- * let obj = { foo: 1, name: "blarg" };
- * let getName = propEq("name", "blarg");
- * getName(obj) === true
- */
-export const propEq = curry((name: string, val: any, obj: any) => obj && obj[name] === val);
-
-/**
- * Given a dotted property name, returns a function that returns a nested property from an object, or undefined
- * let obj = { id: 1, nestedObj: { foo: 1, name: "blarg" }, };
- * let getName = prop("nestedObj.name");
- * getName(obj) === "blarg"
- * let propNotFound = prop("this.property.doesnt.exist");
- * propNotFound(obj) === undefined
- */
-export const parse = (name: string) => pipe.apply(null, name.split(".").map(prop));
-
-/**
- * Given a function that returns a truthy or falsey value, returns a
- * function that returns the opposite (falsey or truthy) value given the same inputs
- */
-export const not = (fn) => (...args) => !fn.apply(null, args);
-
-/**
- * Given two functions that return truthy or falsey values, returns a function that returns truthy
- * if both functions return truthy for the given arguments
- */
-export function and(fn1, fn2): Function {
-  return (...args) => fn1.apply(null, args) && fn2.apply(null, args);
-}
-
-/**
- * Given two functions that return truthy or falsey values, returns a function that returns truthy
- * if at least one of the functions returns truthy for the given arguments
- */
-export function or(fn1, fn2): Function {
-  return (...args) => fn1.apply(null, args) || fn2.apply(null, args);
-}
-
-/** Given a class, returns a Predicate function that returns true if the object is of that class */
-export const is: (ctor) => Predicate<any> = ctor => obj => (obj != null && obj.constructor === ctor || obj instanceof ctor);
-
-/** Given a value, returns a Predicate function that returns true if another value is === equal to the original value */
-export const eq: (comp) => Predicate<any> = (val) => (other) => val === other;
-
-/** Given a value, returns a function which returns the value */
-export const val = <T> (v: T) => () => v;
-
-export function invoke(fnName: string): Function;
-export function invoke(fnName: string, args: any[]): Function;
-export function invoke(fnName: string, args?): Function {
-  return (obj: any) => obj[fnName].apply(obj, args);
-}
-
-/**
- * Sorta like Pattern Matching (a functional programming conditional construct)
- *
- * See http://c2.com/cgi/wiki?PatternMatching
- *
- * This is a conditional construct which allows a series of predicates and output functions
- * to be checked and then applied.  Each predicate receives the input.  If the predicate
- * returns truthy, then its matching output function (mapping function) is provided with
- * the input and, then the result is returned.
- *
- * Each combination (2-tuple) of predicate + output function should be placed in an array
- * of size 2: [ predicate, mapFn ]
- *
- * These 2-tuples should be put in an outer array.
- *
- * @example
- * ```
- *
- * // Here's a 2-tuple where the first element is the isString predicate
- * // and the second element is a function that returns a description of the input
- * let firstTuple = [ angular.isString, (input) => `Heres your string ${input}` ];
- *
- * // Second tuple: predicate "isNumber", mapfn returns a description
- * let secondTuple = [ angular.isNumber, (input) => `(${input}) That's a number!` ];
- *
- * let third = [ (input) => input === null,  (input) => `Oh, null...` ];
- *
- * let fourth = [ (input) => input === undefined,  (input) => `notdefined` ];
- *
- * let descriptionOf = pattern([ firstTuple, secondTuple, third, fourth ]);
- *
- * console.log(descriptionOf(undefined)); // 'notdefined'
- * console.log(descriptionOf(55)); // '(55) That's a number!'
- * console.log(descriptionOf("foo")); // 'Here's your string foo'
- * ```
- *
- * @param struct A 2D array.  Each element of the array should be an array, a 2-tuple,
- * with a Predicate and a mapping/output function
- * @returns {function(any): *}
- */
-export function pattern(struct: Function[][]): Function {
-  return function(x) {
-    for (var i = 0; i < struct.length; i++) {
-      if (struct[i][0](x)) return struct[i][1](x);
-    }
-  };
-}
-
 
 /**
  * prototypal inheritance helper.
@@ -330,7 +147,7 @@ export function merge(dst, ...objs: Object[]) {
   return dst;
 }
 
-/** Reduce function that merges each element of the list into a single object, using angular.extend */
+/** Reduce function that merges each element of the list into a single object, using extend */
 export const mergeR = (memo, item) => extend(memo, item);
 
 /**
@@ -507,6 +324,7 @@ export const values: (<T> (obj: TypedMap<T>) => T[]) = (obj) => Object.keys(obj)
  * ```
  */
 export const allTrueR  = (memo: boolean, elem) => memo && elem;
+
 /**
  * Reduce function that returns true if any of the values are truthy.
  *
@@ -522,8 +340,6 @@ export const allTrueR  = (memo: boolean, elem) => memo && elem;
  */
 export const anyTrueR  = (memo: boolean, elem) => memo || elem;
 
-/** Reduce function that pushes an object to an array, then returns the array.  Mostly just for [[flattenR]] */
-export const pushR     = (arr: any[], obj) => { arr.push(obj); return arr; };
 /**
  * Reduce function which un-nests a single level of arrays
  * @example
@@ -534,6 +350,7 @@ export const pushR     = (arr: any[], obj) => { arr.push(obj); return arr; };
  * ```
  */
 export const unnestR   = (memo: any[], elem) => memo.concat(elem);
+
 /**
  * Reduce function which recursively un-nests all arrays
  *
@@ -545,6 +362,9 @@ export const unnestR   = (memo: any[], elem) => memo.concat(elem);
  * ```
  */
 export const flattenR  = (memo: any[], elem) => isArray(elem) ? memo.concat(elem.reduce(flattenR, [])) : pushR(memo, elem);
+/** Reduce function that pushes an object to an array, then returns the array.  Mostly just for [[flattenR]] */
+function pushR(arr: any[], obj) { arr.push(obj); return arr; }
+
 /**
  * Return a new array with a single level of arrays unnested.
  *
@@ -714,3 +534,64 @@ export function tail(arr: any[]): any {
 }
 
 
+function _toJson(obj) {
+  return JSON.stringify(obj);
+}
+
+function _fromJson(json) {
+  return isString(json) ? JSON.parse(json) : json;
+}
+
+/**
+ * shallow copy from src to dest
+ *
+ * note: This is a shallow copy, while angular.copy is a deep copy.
+ * ui-router uses `copy` only to make copies of state parameters.
+ */
+function _copy(src, dest) {
+  if (dest) Object.keys(dest).forEach(key => delete dest[key]);
+  if (!dest) dest = {};
+  return extend(dest, src);
+}
+
+function _forEach(obj: (any[]|any), cb, _this) {
+  if (isArray(obj)) return obj.forEach(cb, _this);
+  Object.keys(obj).forEach(key => cb(obj[key], key));
+}
+
+function _extend(to, from) {
+  return !from ? to : Object.keys(from).reduce((m, key) => { m[key] = from[key]; return m; }, to);
+}
+
+function _equals(o1, o2) {
+  if (o1 === o2) return true;
+  if (o1 === null || o2 === null) return false;
+  if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
+  let t1 = typeof o1, t2 = typeof o2;
+  if (t1 !== t2 || t1 !== 'object') return false;
+
+  const tup = [o1, o2];
+  if (all(isArray)(tup)) return _arraysEq(o1, o2);
+  if (all(isDate)(tup)) return o1.getTime() === o2.getTime();
+  if (all(isRegExp)(tup)) return o1.toString() === o2.toString();
+  if (all(isFunction)(tup)) return true; // meh
+
+  let predicates = [isFunction, isArray, isDate, isRegExp];
+  if (predicates.map(any).reduce((b, fn) => b || !!fn(tup), false)) return false;
+
+  let key, keys = {};
+  for (key in o1) {
+    if (!_equals(o1[key], o2[key])) return false;
+    keys[key] = true;
+  }
+  for (key in o2) {
+    if (!keys[key]) return false;
+  }
+
+  return true;
+}
+
+function _arraysEq(a1, a2) {
+  if (a1.length !== a2.length) return false;
+  return arrayTuples(a1, a2).reduce((b, t) => b && _equals(t[0], t[1]), true);
+}
---
+++ src/common/predicates.ts
@@ -0,0 +1,12 @@
+const not = (fn) => (x) => !fn(x);
+const tis = (t) => (x) => typeof(x) === t;
+export const isDefined = not(tis('undefined'));
+export const isFunction = tis('function');
+export const isNumber = tis('number');
+export const isString = tis('string');
+export const isObject = (x) => x !== null && typeof x === 'object';
+export const isArray = Array.isArray;
+
+let toStr = Object.prototype.toString;
+export const isDate = (x) => toStr.call(x) === '[object Date]';
+export const isRegExp = (x) => toStr.call(x) === '[object RegExp]';
