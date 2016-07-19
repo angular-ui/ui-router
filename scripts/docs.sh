2512282e19127ffcb649da4eee0f12c286207342
Chris Thielen <github@sandgnat.com>
2016-07-19 16:00:00 -0500

    docs(*): Upgrade to typedoc 0.4.4 and use typedoc plugin external-module-name

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 SCRIPTDIR=`dirname $0`;
 BASEDIR="$SCRIPTDIR/..";
-$BASEDIR/node_modules/typedoc/bin/typedoc --experimentalDecorators --readme $BASEDIR/README.md --name "UI-Router" --theme $BASEDIR/typedoctheme --mode modules --module commonjs --target es5 --out $BASEDIR/_doc  $BASEDIR/src $BASEDIR/typings/es6-shim/es6-shim.d.ts $BASEDIR/typings/angularjs/angular.d.ts
+$BASEDIR/node_modules/typedoc/bin/typedoc $* \
+  --experimentalDecorators \
+  --readme $BASEDIR/README.md \
+  --name "UI-Router" \
+  --theme $BASEDIR/typedoctheme \
+  --mode modules \
+  --module commonjs \
+  --target es5 \
+  --out $BASEDIR/_doc  \
+  $BASEDIR/src $BASEDIR/typings/es6-shim/es6-shim.d.ts $BASEDIR/typings/angularjs/angular.d.ts
