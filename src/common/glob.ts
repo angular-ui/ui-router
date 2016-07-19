ea9c73aa00f2d55b64a663e6d2ccb57600d1c18d
Axar <axar@users.noreply.github.com>
2016-07-19 12:21:13 -0400

    Update glob.ts
    fixed for #2438, prevent matches for *.** based patterns.

--- src/common/glob.ts
+++ src/common/glob.ts
@@ -16,11 +16,6 @@ export class Glob {
   matches(name: string) {
     let segments = name.split('.');
 
-    // match single stars
-    for (let i = 0, l = this.glob.length; i < l; i++) {
-      if (this.glob[i] === '*') segments[i] = '*';
-    }
-
     // match greedy starts
     if (this.glob[0] === '**') {
        segments = segments.slice(segments.indexOf(this.glob[1]));
@@ -31,6 +26,12 @@ export class Glob {
        segments.splice(segments.indexOf(this.glob[this.glob.length - 2]) + 1, Number.MAX_VALUE);
        segments.push('**');
     }
+    
+    // match single stars
+    for (let i = 0, l = this.glob.length; i < l; i++) {
+      if (this.glob[i] === '*' && segments.length > i) segments[i] = '*';
+    }
+
     if (this.glob.length != segments.length) return false;
 
     return segments.join('') === this.glob.join('');
