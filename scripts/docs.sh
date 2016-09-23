b4bc2b0e2ff1f46212f602ddf83b67566a07577b
Chris Thielen <github@sandgnat.com>
2016-09-23 01:18:54 -0500

    temporary kludge for docs gen

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,10 +3,10 @@ SCRIPTDIR=`dirname $0`;
 BASEDIR="$SCRIPTDIR/..";
 
 pushd $BASEDIR;
+mv tsconfig.json tsconfig.json.tmp
 typings install --global dt~es6-shim
 typings install --global dt~angular
 typings install --global dt~jquery
-popd
 
 $BASEDIR/node_modules/typedoc/bin/typedoc $* \
   --experimentalDecorators \
@@ -20,3 +20,6 @@ $BASEDIR/node_modules/typedoc/bin/typedoc $* \
   $BASEDIR/src $BASEDIR/typings/index.d.ts
 
   rm -rf $BASEDIR/typings
+
+mv tsconfig.json.tmp tsconfig.json
+popd
