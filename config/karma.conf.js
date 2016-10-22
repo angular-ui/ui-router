9f25097c4fabe8c32a4cbfeaac22a4638c1b3943
Chris Thielen <github@sandgnat.com>
2016-10-22 11:53:35 -0500

    test(ng1): simplify ng1 karma configuration

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,48 +1,91 @@
 // Karma configuration file
-
 var karma = require("karma");
-var files = require('../files').files;
 
-var config = {
+/**
+ * This returns a Karma 'files configuration'.
+ * http://karma-runner.github.io/0.8/config/files.html
+ *
+ * Specifies which files can be served by the Karma web server
+ *
+ * included: true -- files that are always served to the browser (like <script> tag)
+ * included: false -- files *available to be served* by karma, for instance via require()
+ */
+function karmaServedFiles(ngVersion) {
+  // Returns necessary files for a specific version of angular
+  function angular(version) {
+    console.log('Using Angular ' + ngVersion + ' from test/angular/' + version + '/angular.js');
 
-  singleRun: true,
-  autoWatch: false,
-  autoWatchInterval: 0,
+    return [
+      'test/angular/' + version + '/angular.js',
+      'test/angular/' + version + '/angular-mocks.js',
+      'test/angular/' + version + '/angular-animate.js',
+      'test/util/ng1.systemjs.adapter.js'
+    ];
+  }
 
-  // level of logging
-  // possible values: LOG_DISABLE, LOG_ERROR, LOG_WARN, LOG_INFO, LOG_DEBUG
-  logLevel: "warn",
-  // possible values: 'dots', 'progress'
-  reporters: 'dots',
-  colors: true,
+  let angularFiles = angular(ngVersion).map(function (pattern) {
+    return { watched: false, included: true, nocache: true, pattern: pattern };
+  });
 
-  port: 8080,
+  let srcFiles = [
+    { watched: true, included: false, nocache: true, pattern: 'src/**/*.ts' },
+  ];
 
-  // base path, that will be used to resolve files and exclude
-  basePath: '..',
+  let testFiles = [
+    { watched: true, included: false, nocache: true, pattern: 'test/**/*.ts' },
+    { watched: true, included: false, nocache: true, pattern: 'test/**/*.js' }
+  ];
 
-  // Start these browsers, currently available:
-  // Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
-  browsers: ['PhantomJS'],
+  return [].concat(angularFiles).concat(srcFiles).concat(testFiles);
+}
 
-  frameworks: ['systemjs', 'jasmine'],
+module.exports = function(config) {
+  var ngVersion = config.ngversion || "1.5.0";
 
-  plugins: [
-    require('karma-systemjs'),
-    require('karma-jasmine'),
-    require('karma-phantomjs-launcher'),
-    require('karma-chrome-launcher')
-  ],
+  config.set({
+    singleRun: true,
+    autoWatch: false,
+    autoWatchInterval: 0,
 
-  systemjs: {
-    // Set up systemjs paths
-    configFile: 'config/system.config.js',
-    files: [
+    // level of logging
+    // possible values: LOG_DISABLE, LOG_ERROR, LOG_WARN, LOG_INFO, LOG_DEBUG
+    logLevel: "warn",
+    // possible values: 'dots', 'progress'
+    reporters: 'dots',
+    colors: true,
+
+    port: 8080,
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '.',
+
+    // Start these browsers, currently available:
+    // Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
+    browsers: ['PhantomJS'],
+
+    frameworks: ['systemjs', 'jasmine'],
+
+    plugins: [
+      require('karma-systemjs'),
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher')
+    ],
+
+    /* Files *available to be served* by karma, i.e., anything that will be require()'d */
+    files: karmaServedFiles(ngVersion),
+    exclude: [],
+    systemjs: {
+      // Set up systemjs paths
+      configFile: 'karma.system.config.js',
+
+      files: [
         'src/**/*.ts',
         'node_modules/ui-router-core/lib/**/*'
-    ]
-  },
-  exclude: []
-};
+      ],
 
-module.exports = config;
+      // karma-systemjs kludge: This is turned into a regexp and is the actual specs that are loaded
+      testFileSuffix: "/test/\\S+Spec.[tj]s"
+    },
+  });
+};
--- config/karma.ng13.js
+++
@@ -1,4 +0,0 @@
-var ng1ConfigFactory = require("./_karma.ng1");
-module.exports = function (karma) {
-  karma.set(ng1ConfigFactory("1.3.16", "/test/ng1/\\S+.[tj]s"));
-};
