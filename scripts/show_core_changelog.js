c66885f9b9327cb971f3e18d1811e9a940dc7c06
Chris Thielen <github@sandgnat.com>
2017-01-17 08:40:39 -0600

    chore(scripts): Print better errors from /scripts/show_core_changelog.js

--- scripts/show_core_changelog.js
+++ scripts/show_core_changelog.js
@@ -3,22 +3,53 @@
 var gitSemverTags = require('git-semver-tags');
 var shelljs = require('shelljs');
 var path = require('path');
+var fs = require('fs');
+
+var CORE_PKG = 'ui-router-core';
+var CORE_DIR = path.join(__dirname, "..", "..", CORE_PKG);
+var SHOWCHANGELOG_SCRIPT = path.join(CORE_DIR, "scripts", "show_changelog.js");
+
+var currentPackage = require('../package.json');
+if (!currentPackage.dependencies || !currentPackage.dependencies[CORE_PKG]) {
+  throw new Error("No dependency on " + CORE_PKG + " found in package.json.")
+}
+
+if (!fs.existsSync(CORE_DIR)) {
+  throw new Error(CORE_PKG + " not found at " + path.resolve(CORE_DIR));
+}
+
+if (!fs.existsSync(SHOWCHANGELOG_SCRIPT)) {
+  throw new Error("show_changelog.js not found at " + path.resolve(SHOWCHANGELOG_SCRIPT));
+}
 
 shelljs.config.silent = true;
 gitSemverTags(function (err, val) {
   var fromTag;
-  if(require.main === module) {
-    if (process.argv[2]) {
-      fromTag = process.argv[2];
-    }
+  if (require.main === module && process.argv[2]) {
+    fromTag = process.argv[2];
   } else {
-    var prev = shelljs.exec('git show ' + val[0] + ':package.json', {silent:true}).stdout;
-    fromTag = JSON.parse(prev).dependencies['ui-router-core'].replace(/[=~^]/, "");
+    var prevRaw = shelljs.exec('git show ' + val[0] + ':package.json', {silent:true}).stdout;
+    var prevPackage;
+    try {
+      prevPackage = JSON.parse(prevRaw);
+    } catch (error) {
+      console.error("Unable to parse previous package.json");
+      console.error(prevRaw);
+      throw error;
+    }
+
+    if (!prevPackage.dependencies) {
+      throw new Error("previous package.json has no `dependencies` key.");
+    } else if (!prevPackage.dependencies[CORE_PKG]) {
+      throw new Error("previous package.json has no `dependencies['ui-router-core']` key.");
+    }
+
+    fromTag = prevPackage.dependencies[CORE_PKG].replace(/[=~^]/, "");
   }
 
-  var toTag = require("../package.json").dependencies['ui-router-core'].replace(/[=~^]/g, "");
-  shelljs.pushd(path.join(__dirname, "../../ui-router-core"));
-  // console.log("node " + path.join(__dirname, "../../ui-router-core/scripts/show_changelog.js") + " " + fromTag + " " + toTag)
+  var toTag = require("../package.json").dependencies[CORE_PKG].replace(/[=~^]/g, "");
+  shelljs.pushd(CORE_DIR);
+  // console.log("node " + SHOWCHANGELOG_SCRIPT + " " + fromTag + " " + toTag)
   shelljs.config.silent = false;
-  shelljs.exec("node " + path.join(__dirname, "../../ui-router-core/scripts/show_changelog.js") + " " + fromTag + " " + toTag)
+  shelljs.exec("node " + SHOWCHANGELOG_SCRIPT + " " + fromTag + " " + toTag)
 });
