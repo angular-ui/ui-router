ef5ce80fa6ec8a2c6a87cf1cf1c5809e1950af11
christopherthielen <github@sandgnat.com>
2017-01-20 11:09:46 -0600

    release 0.4.2

--- release/angular-ui-router.js
+++ release/angular-ui-router.js
@@ -1,6 +1,6 @@
 /**
  * State-based routing for AngularJS
- * @version v0.4.1
+ * @version v0.4.2
  * @link http://angular-ui.github.com/
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
@@ -3467,7 +3467,7 @@ function $StateProvider(   $urlRouterProvider,   $urlMatcherFactory) {
         return $q.reject(error);
       });
 
-      transition.catch(angular.noop);
+      silenceUncaughtInPromise(transition);
       return transition;
     };
 
