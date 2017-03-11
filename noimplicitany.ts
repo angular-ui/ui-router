8e7386b05bf48c39c1e31e58a86a6ec0e4fc3586
Chris Thielen <github@sandgnat.com>
2017-03-10 17:26:01 -0700

    chore(typescript): Add noImplicitAny compliance check in `test` script
    Closes https://github.com/angular-ui/ui-router/issues/2693

---
+++ noimplicitany.ts
@@ -0,0 +1,5 @@
+// This file should be compiled using --noImplicitAny to ensure the .d.ts files are noImplicitAny compliant.
+// tsc --noEmit --noImplicitAny --moduleResolution node --target es6 test/noimplicitany.ts
+
+import * as router from  "../lib/index";
+console.log(router);
