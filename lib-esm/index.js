47baf3d998674aef09674382262a7723d7f3e0f8
Austin McDaniel <amcdaniel2@gmail.com>
2017-03-30 17:07:18 -0400

    chore(*): commiting build files

---
+++ lib-esm/index.js
@@ -0,0 +1,18 @@
+/**
+ * Main entry point for angular 1.x build
+ * @module ng1
+ */ /** */
+import * as core from "ui-router-core";
+export { core };
+export * from "ui-router-core";
+export * from "./services";
+export * from "./statebuilders/views";
+export * from "./stateProvider";
+export * from "./urlRouterProvider";
+import "./injectables";
+import "./directives/stateDirectives";
+import "./stateFilters";
+import "./directives/viewDirective";
+import "./viewScroll";
+export default "ui.router";
+//# sourceMappingURL=index.js.map
---
+++ lib-esm/legacy/resolveService.d.ts
@@ -0,0 +1,6 @@
+/** @hidden */
+export declare const resolveFactory: () => {
+    resolve: (invocables: {
+        [key: string]: Function;
+    }, locals?: {}, parent?: Promise<any>) => Promise<any>;
+};
