fc7d9b2aff5e5545bffeb4094d402ef25ffd7008
Chris Thielen <cthielen@netflix.com>
2017-05-03 20:22:14 -0700

    chore(release): use ssh instead of https:// when publishing to bower

--- scripts/release.js
+++ scripts/release.js
@@ -55,7 +55,7 @@ _exec(`git checkout -b bower-${version}`);
 _exec(`git add --force release`);
 _exec(`git commit -m "bower release ${version}"`);
 _exec(`git tag ${version}+bower`);
-_exec(`git remote add bower https://github.com/angular-ui/angular-ui-router-bower.git`);
+_exec(`git remote add bower git@github.com:angular-ui/angular-ui-router-bower.git`);
 _exec(`git push bower ${version}+bower:${version}`);
 _exec(`git remote rm bower`);
 _exec(`git checkout master`);
