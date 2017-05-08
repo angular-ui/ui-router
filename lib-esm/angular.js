9722b699aa0fd8d82bdc5b8ad7421c85de16d1b3
Chris Thielen <cthielen@netflix.com>
2017-05-08 11:42:49 -0700

    chore(*): commiting build files

---
+++ lib-esm/angular.js
@@ -0,0 +1,4 @@
+import * as ng_from_import from "angular";
+var ng_from_global = angular;
+export var ng = (ng_from_import && ng_from_import.module) ? ng_from_import : ng_from_global;
+//# sourceMappingURL=angular.js.map
