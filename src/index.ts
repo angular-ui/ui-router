befb50a2421df48a5830f17f8243ab0b13aebbf2
Chris Thielen <cthielen@netflix.com>
2017-05-08 11:42:01 -0700

    fix(bundle): work around rollup bug https://github.com/rollup/rollup/issues/1322

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@
  * @module ng1
  */ /** */
 
-import * as core from "@uirouter/core";
-export { core };
-export * from "@uirouter/core";
-
 export * from "./interface";
 export * from "./services";
 export * from "./statebuilders/views";
@@ -20,3 +16,8 @@ import "./directives/viewDirective";
 import "./viewScroll";
 
 export default "ui.router";
+
+import * as core from "@uirouter/core";
+export { core };
+export * from "@uirouter/core";
+
