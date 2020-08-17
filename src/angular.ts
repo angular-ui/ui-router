eebe4522ed8c45f928fb1b49bb77cff4fd95e686
Zack Schuster <email@zackschuster.com>
2020-08-16 19:04:53 -0700

    fix: drop ng_from_import to avoid webpack warning

--- src/angular.ts
+++ src/angular.ts
@@ -1,5 +1,4 @@
 /** @publicapi @module ng1 */ /** */
-import * as ng_from_import from 'angular';
 /** @hidden */ declare let angular;
 /** @hidden */ const ng_from_global = angular;
-/** @hidden */ export const ng = ng_from_import && ng_from_import.module ? ng_from_import : ng_from_global;
+/** @hidden */ export const ng = ng_from_global;
